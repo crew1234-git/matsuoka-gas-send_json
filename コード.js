1ccde8f95ee1b7afae9e1d822812066934c2bc1b
crew1234-git <suzuki.ne.jp@gmail.com>
2024-03-27 00:58:39 +0900

    A列が空白だった場合は除外するように修正

--- コード.js
+++ コード.js
@@ -1,5 +1,5 @@
 const TOKEN = "cJrAD9Es62qm0Z3eE6uzQQe2dpc1t8QnWZrHOMgMb";
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwnpo2mo94MqRuGFCd1pO92e34v_tGMwqRgKQ6VvxxAohFHo_A5QCs44Gc6XCcjOwOxYg/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbws8B77ADDrBTl59bUMSoeq1iEIlAbB3-EpG2VAYM2faakq6tiShpDx0-chLkpMxi9UNw/exec";
 
 const postSpreadsheetData = () => {
   const ui = SpreadsheetApp.getUi(); // UIインスタンスの取得
@@ -10,13 +10,15 @@ const postSpreadsheetData = () => {
       const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
       const lastRow = sheet.getLastRow();
       
-      // A列を取得
+      // A列とB列のデータを取得
       const columnAData = sheet.getRange("A2:A" + lastRow).getValues();
-      
-      // B列を取得し、空欄の場合は0を代入
       const columnBData = sheet.getRange("B2:B" + lastRow).getValues().map(row => row[0] === "" ? [0] : row);
       
-      const data = columnAData.map((row, index) => [row[0], columnBData[index][0]]);
+      // A列が空でない行だけをフィルタリング
+      const data = columnAData
+        .map((row, index) => [row[0], columnBData[index][0]])
+        .filter(row => row[0] !== ""); // A列が空の行を除外
+
       const filenamePrefix = sheet.getRange("C2").getValue(); // C列の2行目
       
       const payload = {
@@ -41,4 +43,3 @@ const postSpreadsheetData = () => {
     ui.alert('送信がキャンセルされました。');
   }
 };
-
